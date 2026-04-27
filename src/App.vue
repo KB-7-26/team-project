cee2726558c8c8a7f3705953079593f71cea626d
catlover <jimini1183@naver.com>
2026-04-27 23:44:32 +0900

    feat: TopNavBar 컴포넌트 구현 및 디자인 시스템 설정

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import TopNavBar from './components/common/TopNavBar.vue'
 </script>
 
 <template>
+  <TopNavBar />
   <RouterView />
 </template>
