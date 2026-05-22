08f92b3f91960dd4c063aa3b405c6209a839b390
kskim <kskim0109@gmail.com>
2026-05-22 14:29:30 +0900

    feat: 게시글 목록 페이지 API 연동 (#41)

--- src/api/boardApi.js
+++ src/api/boardApi.js
@@ -1,9 +1,12 @@
-import axios from 'axios'
+import api from './axios'
 
-const BASE_URL = '/api/posts'
+const BASE_URL = '/posts'
 
 export const boardApi = {
+  getPosts(page = 0, size = 10) {
+    return api.get(BASE_URL, { params: { page, size } })
+  },
   createPost(title, content, isAnonymous) {
-    return axios.post(BASE_URL, { title, content, isAnonymous })
+    return api.post(BASE_URL, { title, content, isAnonymous })
   },
 }
