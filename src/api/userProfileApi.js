7d93787071fa7d516127f09c86db82719ea834c3
Armont52 <102473445+Armont52@users.noreply.github.com>
2026-06-30 14:33:09 +0900

    feat: 프로필 이미지 업로드 기능 연결

--- src/api/userProfileApi.js
+++ src/api/userProfileApi.js
@@ -12,4 +12,12 @@ export const userProfileApi = {
   updateMyProfile(body) {
     return api.put('/users/me/profile', body)
   },
+
+  uploadProfileImage(formData) {
+    return api.post('/users/me/profile-image', formData)
+  },
+
+  deleteProfileImage() {
+    return api.delete('/users/me/profile-image')
+  },
 }
